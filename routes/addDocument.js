0bc345a78a8643152a460eefb648e061375741e1
kit <kirsh.ru@yandex.ru>
2019-05-06 12:58:14 +0700

    new doc page work and fix error page

--- routes/addDocument.js
+++ routes/addDocument.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+var moment = require('moment');
 const Document = require('../database/models/Document')
 const DocSigners = require('../database/models/DocSigners')
 const User = require('../database/models/User')
@@ -9,39 +10,87 @@ const Position = require('../database/models/Position')
 const DocType = require('../database/models/DocType')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('addDocument',
-        { });
-});
-router.get('/add', async function (req, res) {
-    let doc = await Client.create({
-        FirstName: req.body.inputName,
-        SecondName: req.body.inputSecondName,
-        Patronymic: req.body.inputPatronymic,
-        PhoneNumber: req.body.inputPhoneNumber,
-        Adress_fias: req.body.inputAdress,
-        Adress: req.body.address,
-        DLNumber: req.body.inputDLN,
-        Birthday: moment(req.body.inputBD, 'DD.MM.YYYY').startOf('day')
+router.get('/',async function(req, res, next) {
+    let document
+
+    if (req.query.ID === "" || req.query.ID === undefined) {
+        document = await Document.create({
+        })
+            .catch((err) => {
+                console.log(err)
+            })
+
+    }
+    else {
+        document = await Document.findOne({
+
+            include: [{model: DocType, as: 'DocType'},
+                {model: User, as: 'User'}],
+            where: {id: req.query.ID}
+        })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    let signers = await DocSigners.findAll({
+        include: [{model: User, as: 'User'}],
+        where: {DocumentFK: document.dataValues.id}
     })
         .catch((err) => {
             console.log(err)
         })
 
+    let type = await DocType.findAll()
+    let users = await User.findAll()
+    res.render('addDocument',
+        {
+            type: type,
+            document: document,
+            users: users,
+            signers: signers,
+        });
+});
+
+router.post('/add', async function (req, res) {
+    let type = await DocType.findOne({
+        where:{Name: req.body.inputType}
+    })
+        .catch((err) => {
+            console.log(err)
+        })
+    let doc = await Document.update({
+        Name: req.body.inputName,
+        Number: req.body.inputNum,
+        TypeFK: type.dataValues.id,
+        AuthorFK: 2,
+        Text: req.body.inputText,
+        Date: moment(req.body.inputDate, 'YYYY-MM-DD').startOf('day')},
+        {where:{id:req.query.id}
+    })
+        .catch((err) => {
+            console.log(err)
+        })
+
+})
 
 
-    if (req.query.SLID === undefined) {
-        res.redirect(req.headers.referer)
-    }
-    else {
-        let sl = await ServiceList.update({
-            ClientFK: cl.dataValues.id
-        }, {where: {id: req.query.SLID}})
-            .catch((err) => {
-                console.log(err)
-            })
-        res.redirect('/addservicelist?ID=' + req.query.SLID)
-    }
+router.post('/addSigner', async function (req, res) {
+    let user= await User.findOne({
+        where:{SecondName:req.body.inputName.split(" ")[1]}
+    })
+        .catch((err) => {
+            console.log(err)
+        })
+    await DocSigners.create({
+        DocumentFK: req.query.ID,
+        UserFK: user.dataValues.id,
+        SignedFK: 4
+    })
+        .catch((err) => {
+            console.log(err)
+        })
+    res.redirect('/addDocument?ID='+req.query.ID)
 
 })
 module.exports = router;
