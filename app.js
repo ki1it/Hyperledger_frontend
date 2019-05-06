782452177f4674b98a39ca728221081b737820cb
kit <kirsh.ru@yandex.ru>
2019-05-06 19:26:43 +0700

    logout done

--- app.js
+++ app.js
@@ -133,12 +133,8 @@ app.get('/dashboard', (req, res) => {
 
 // route for user logout
 app.get('/logout', (req, res) => {
-  if (req.session.user && req.cookies.user_sid) {
     res.clearCookie('user_sid');
     res.redirect('/');
-  } else {
-    res.redirect('/login');
-  }
 });
 
 
