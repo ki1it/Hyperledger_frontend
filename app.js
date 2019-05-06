ca0c20f359463a38bd3f449b54d328562c2f9fc5
kit <kirsh.ru@yandex.ru>
2019-05-06 13:06:31 +0700

    new doc page work and login page

--- app.js
+++ app.js
@@ -20,7 +20,124 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var morgan = require('morgan');
+// set morgan to log info about our requests for development use.
+app.use(morgan('dev'));
+
+// initialize body-parser to parse incoming parameters requests to req.body
+app.use(bodyParser.urlencoded({extended: true}));
+// initialize express-session to allow us track the logged-in user across sessions.
+app.use(session({
+  key: 'user_sid',
+  secret: 'somerandonstuffs',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: 600000
+  }
+}));
+// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
+// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
+app.use((req, res, next) => {
+  if (req.cookies.user_sid && !req.session.user) {
+    res.clearCookie('user_sid');
+  }
+  next();
+});
+
+
+// middleware function to check for logged-in users
+var sessionChecker = (req, res, next) => {
+  if (req.session.user && req.cookies.user_sid) {
+    res.redirect(req.originalUrl);
+  } else {
+    next();
+  }
+};
+//end of auth part
+
+
+
+app.get('/', sessionChecker, (req, res) => {
+  res.redirect('/login');
+})
+// route for user signup
+app.route('/signup')
+    .get((req, res) => {
+      res.render(__dirname + '/views/signup.pug');
+    })
+    .post((req, res) => {
+      User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        role: req.body.role,
+        worker_id: req.body.wid
+      })
+          .then(user => {
+            req.session.user = user.dataValues;
+            res.redirect('/signup');
+          })
+          .catch(error => {
+            res.redirect('/signup');
+          });
+    });
+
+
+// route for user Login
+app.route('/login')
+    .get(sessionChecker, (req, res) => {
+      res.render(__dirname + '/views/login.pug');
+    })
+    .post((req, res) => {
+      var username = req.body.username,
+          password = req.body.password;
+
+      User.findOne({where: {username: username}}).then(function (user) {
+        if (!user) {
+          res.redirect('/login');
+        } else if (user.dataValues.password!==password) {
+          res.redirect('/login');
+        } else {
+          req.session.user = user.dataValues;
+          if (user.dataValues.role === 'admin') {
+            res.redirect('/signup')
+          }
+          if (user.dataValues.role === 'administrator') {
+            res.redirect('/index')
+          }
+          // if (user.dataValues.role === 'worker') {
+          //     res.redirect('/zadForWorker?id=' + user.dataValues.worker_id)
+          // }
+        }
+      });
+    });
+
+
+// route for user's dashboard
+app.get('/dashboard', (req, res) => {
+  if (req.session.user && req.cookies.user_sid) {
+    res.sendFile(__dirname + '/public/dashboard.html');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+
+// route for user logout
+app.get('/logout', (req, res) => {
+  if (req.session.user && req.cookies.user_sid) {
+    res.clearCookie('user_sid');
+    res.redirect('/');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 app.use('/addDocument', addDocumentRouter);
 
